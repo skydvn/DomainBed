d7924c0144f566b67ad0b615bf49922a670bfa7d
Daniel Nguyen <mduongbkhn@gmail.com>
2024-01-30 09:38:55 +0700

    LKD

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -22,6 +22,8 @@ from domainbed.lib.misc import (
 
 
 ALGORITHMS = [
+    'LKD', # Label-driven KD
+    'CAG', # CA Grad
     'ERM',
     'Fish',
     'IRM',
@@ -128,6 +130,65 @@ class Fish(Algorithm):
 
     def __init__(self, input_shape, num_classes, num_domains, hparams):
         super(Fish, self).__init__(input_shape, num_classes, num_domains,
+                                   hparams)
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+
+        self.network = networks.WholeFish(input_shape, num_classes, hparams)
+        self.optimizer = torch.optim.Adam(
+            self.network.parameters(),
+            lr=self.hparams["lr"],
+            weight_decay=self.hparams['weight_decay']
+        )
+        self.optimizer_inner_state = None
+
+    def create_clone(self, device):
+        self.network_inner = networks.WholeFish(self.input_shape, self.num_classes, self.hparams,
+                                            weights=self.network.state_dict()).to(device)
+        self.optimizer_inner = torch.optim.Adam(
+            self.network_inner.parameters(),
+            lr=self.hparams["lr"],
+            weight_decay=self.hparams['weight_decay']
+        )
+        if self.optimizer_inner_state is not None:
+            self.optimizer_inner.load_state_dict(self.optimizer_inner_state)
+
+    def fish(self, meta_weights, inner_weights, lr_meta):
+        meta_weights = ParamDict(meta_weights)
+        inner_weights = ParamDict(inner_weights)
+        meta_weights += lr_meta * (inner_weights - meta_weights)
+        return meta_weights
+
+    def update(self, minibatches, unlabeled=None):
+        self.create_clone(minibatches[0][0].device)
+
+        for x, y in minibatches:
+            loss = F.cross_entropy(self.network_inner(x), y)
+            self.optimizer_inner.zero_grad()
+            loss.backward()
+            self.optimizer_inner.step()
+
+        self.optimizer_inner_state = self.optimizer_inner.state_dict()
+        meta_weights = self.fish(
+            meta_weights=self.network.state_dict(),
+            inner_weights=self.network_inner.state_dict(),
+            lr_meta=self.hparams["meta_lr"]
+        )
+        self.network.reset_weights(meta_weights)
+
+        return {'loss': loss.item()}
+
+    def predict(self, x):
+        return self.network(x)
+
+
+class LKD(Algorithm):
+    """
+    Implementation of LKD, as seen in Label-driven Knowledge Distillation.
+    """
+
+    def __init__(self, input_shape, num_classes, num_domains, hparams):
+        super(LKD, self).__init__(input_shape, num_classes, num_domains,
                                    hparams)
         self.input_shape = input_shape
         self.num_classes = num_classes
